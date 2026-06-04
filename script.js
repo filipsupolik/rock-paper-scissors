6b53e365ded8261d56ba00a24d1c7cf7a31c794d
fisu01 <filipsupolik25@gmail.com>
2026-06-04 16:41:06 +0200

    RPS app UI is working with dynamic results and background changes that responds to result of 1 match

--- script.js
+++ script.js
@@ -10,48 +10,85 @@ let getComputerChoice = () => {
   }
 };
 
-let getHumanChoice = () => {
-  let input = "".toLowerCase();
-};
+function textPropertySetter(element) {
+  element.style.padding = "8px";
+  element.style.fontStyle = "bold";
+  element.style.fontSize = "20px";
+}
 
 function playGame() {
   const btnRock = document.querySelector(".rock");
   const btnPaper = document.querySelector(".paper");
   const btnScissors = document.querySelector(".scissors");
 
+  const buttons = document.getElementsByTagName("button");
+
+  const resultsContainer = document.querySelector(".result-container");
+  resultsContainer.style.border = "3px solid black";
+  resultsContainer.style.margin = "16px";
+  resultsContainer.style.display = "flex";
+  resultsContainer.style.justifyContent = "space-around";
+  resultsContainer.style.alignItems = "center";
+
+  const results = document.querySelector(".content");
+  textPropertySetter(results);
+
+  const runningScore = document.querySelector(".runningScore");
+  textPropertySetter(runningScore);
+
   let humanScore = 0,
     computerScore = 0;
 
-  function playRound() {
-    let humanChoiceLower = getHumanChoice();
+  function reset() {
+    resultsContainer.style.backgroundColor = "#fff";
+    results.textContent = "";
+    Array.from(buttons).forEach((element) => {
+      element.disabled = true;
+    });
+    humanScore = 0;
+    computerScore = 0;
+  }
+
+  function playRound(playerSelection) {
+    let humanChoiceLower = playerSelection;
     let computerChoice = getComputerChoice();
 
     if (humanChoiceLower === computerChoice) {
-      console.log("It's tie!");
+      results.textContent = "It's tie!";
+      resultsContainer.style.backgroundColor = "#fff";
     } else if (
       (humanChoiceLower === "rock" && computerChoice === "scissors") ||
       (humanChoiceLower === "paper" && computerChoice === "rock") ||
       (humanChoiceLower === "scissors" && computerChoice === "paper")
     ) {
       humanScore++;
-      console.log(`You win! ${humanChoiceLower} beats ${computerChoice}`);
+      results.textContent = `You win! ${humanChoiceLower} beats ${computerChoice}`;
+      resultsContainer.style.backgroundColor = "#00ff26de";
     } else {
       computerScore++;
-      console.log(`You lose! ${computerChoice} beats ${humanChoiceLower}`);
+      results.textContent = `You lose! ${computerChoice} beats ${humanChoiceLower}`;
+      resultsContainer.style.backgroundColor = "#f83737";
     }
-    console.log(`Score: human: ${humanScore} || computer: ${computerScore}`);
+    runningScore.textContent = `Score: human: ${humanScore} || computer: ${computerScore}`;
+    if (humanScore === 5) {
+      runningScore.textContent = "Winner is human!";
+      reset();
+    } else if (computerScore === 5) {
+      runningScore.textContent = "Winner is computer!";
+      reset();
+    }
   }
 
   btnRock.addEventListener("click", (e) => {
-    playRound();
+    playRound("rock");
   });
 
   btnPaper.addEventListener("click", (e) => {
-    playRound();
+    playRound("paper");
   });
 
   btnScissors.addEventListener("click", (e) => {
-    playRound();
+    playRound("scissors");
   });
 }
 
